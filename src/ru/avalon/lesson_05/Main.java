00ea1453cd4b18ad309d781051409e0ba067f004
Crevan <a.siverskii@gmail.com>
2022-03-31 21:32:19 +0300

    update main for boolean array

--- src/ru/avalon/lesson_05/Main.java
+++ src/ru/avalon/lesson_05/Main.java
@@ -16,6 +16,20 @@ public class Main {
         System.out.println(binary);
         binary.setFalse(1);
         System.out.println(binary);
+
+        IBool logic = new Logical();
+
+        System.out.println(logic);
+        logic.setTrue(1);
+        logic.setTrue(2);
+        System.out.println(logic);
+        logic.setFalse(2);
+        System.out.println(logic);
+        logic.inversion(1);
+        System.out.println(logic);
+        logic.setTrue(6);
+        System.out.println(logic.getByIndex(6));
+
     }
 
 }
