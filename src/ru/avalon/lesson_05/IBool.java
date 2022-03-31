12cfced19d9a2a77edf6ba3df5bca7e4a4f7da70
Crevan <a.siverskii@gmail.com>
2022-03-31 20:55:28 +0300

    add interface IBool

---
+++ src/ru/avalon/lesson_05/IBool.java
@@ -0,0 +1,10 @@
+package ru.avalon.lesson_05;
+
+public interface IBool {
+
+    boolean getByIndex(int index);
+    void inversion(int index);
+    void setTrue(int index);
+    void setFalse(int index);
+
+}
