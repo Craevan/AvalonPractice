a653523fbd81e023e484d972b546f1319ac8a231
Crevan <a.siverskii@gmail.com>
2022-03-31 20:57:08 +0300

    binary variable type changing to IBool

--- src/ru/avalon/lesson_05/Main.java
+++ src/ru/avalon/lesson_05/Main.java
@@ -3,15 +3,18 @@ package ru.avalon.lesson_05;
 public class Main {
 
     public static void main(String[] args) {
-        Binary binary = new Binary();
+        IBool binary = new Binary();
 
         System.out.println(binary);
         binary.inversion(4);
         System.out.println(binary);
-        binary.setTrue(3);
+        binary.setTrue(1);
+        binary.setTrue(1);
         binary.setTrue(3);
         System.out.println(binary);
         binary.inversion(3);
+        System.out.println(binary);
+        binary.setFalse(1);
         System.out.println(binary);
     }
 
